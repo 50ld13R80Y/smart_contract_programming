0c71088bb9c989cf593248949e3d537c2a5e128a
RJ <32805187+ryanjonesrocks@users.noreply.github.com>
2022-05-30 22:04:34 -0700

    Create FundMe.sol

---
+++ FundMe.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
+
+//   function description() external view returns (string memory);
+
+//   function version() external view returns (uint256);
+
+//   // getRoundData and latestRoundData should both raise "No data present"
+//   // if they do not have data to report, instead of returning unset values
+//   // which could be misinterpreted as actual reported values.
+//   function getRoundData(uint80 _roundId)
+//     external
+//     view
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+
+//   function latestRoundData()
+//     external
+//     view
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+// }
+
+contract FundMe {
+
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        // $50 
+        uint256 minimumUSD = 50 * 10 * 18;
+        // msg.sender - sender of the function call
+        // msg.value - how much was funded
+        addressToAmountFunded[msg.sender] += msg.value;
+
+        //what is ETH/USD conversion rates
+    }
+
+    function getVersion() public view returns (uint256) {
+        //https://docs.chain.link/docs/ethereum-addresses/
+        //Rinkby
+        //ETH / USD 	0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version(); //version of AggregatorV3Interface
+    }
+
+    // Return current price of ethereum
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        //Display price in Wei
+        return uint256(answer * 10000000000);
+    }
+
+    //Convert ETH to USD
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 10**18;
+        return ethAmountInUsd;
+    }
+}
